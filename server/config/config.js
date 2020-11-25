fd2068a657042e2c50129a5a64e75f10536c68d8
Pierre <pierreaqv@hotmail.com>
2020-11-25 14:02:09 -0500

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -18,10 +18,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //======================
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//   urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://pierre:m9QrrLjcwtWiwkzb@cluster0.m2imy.mongodb.net/cafe';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://pierre:m9QrrLjcwtWiwkzb@cluster0.m2imy.mongodb.net/cafe';
+}
 
 process.env.URLDB = urlDB;
