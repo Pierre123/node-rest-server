889cc2a4ce37e7034aa2f3756481e0533ee4cf61
Pierre <pierreaqv@hotmail.com>
2020-11-25 21:51:39 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://pierre:m9QrrLjcwtWiwkzb@cluster0.m2imy.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
