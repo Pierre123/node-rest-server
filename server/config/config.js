65cca0e62f6440cd58bb5fdecac36d8146016c75
Pierre <pierreaqv@hotmail.com>
2020-12-07 13:02:34 -0500

    google Sign In

--- server/config/config.js
+++ server/config/config.js
@@ -38,3 +38,10 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 //SEED de autenticacion
 //======================
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+
+//======================
+//Google ClienteID
+//======================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '133358281178-7cd19rm8375i41a7sk5krt9hi3db5qff.apps.googleusercontent.com';
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -12,7 +13,11 @@ const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+//habilitar carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
+//console.log(path.resolve(__dirname, '../public'));
 
 // Configuracion global de rutas
 app.use(require('./routes/index.js'));
